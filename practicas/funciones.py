bd896523afadef5d0091f8105017117513aed1f0
Kaiser Fox <julio.ros.per@gmail.com>
2022-04-21 19:07:08 -0500

    Practicas agregado

---
+++ practicas/funciones.py
@@ -0,0 +1,12 @@
+def sumar_algo(*args):
+    '''
+    Esta funcion suma un numero indetermidado de numeros
+    y lo almaneca en la variable resultado
+    return= resultado
+    '''
+    resultado=0
+    print(type(args))
+
+
+    sumar_algo(4,5,6,7)
+
