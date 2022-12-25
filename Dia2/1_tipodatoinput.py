527494604d806ea0e1122ec0fe6a0df2af464c69
Kaiser Fox <julio.ros.per@gmail.com>
2022-12-25 14:09:25 -0600

    modificaciones en Día2

--- Dia2/1_tipodatoinput.py
+++ Dia2/1_tipodatoinput.py
@@ -1,26 +1,36 @@
-num1 = 2.8
-print(num1)
-print(type(num1))
-num2 = int(num1)
-print(num2)
-print(type(num2))
+def input_variables():
+    """
+    Input y tipos de variables
+    """
+    num1 = 2.8
+    print(num1)
+    print(type(num1))
+    num2 = int(num1)
+    print(num2)
+    print(type(num2))
 
-edad = input("escribe tu edad")
-print(type(edad))
-nedad = int(edad)
-print(type(edad))
-print(type(nedad))
-nedad = nedad + nedad
-print(nedad)
+    edad = input("escribe tu edad")
+    print(type(edad))
+    nedad = int(edad)
+    print(type(edad))
+    print(type(nedad))
+    nedad = nedad + nedad
+    print(nedad)
 
-num1 = 7.5
-num1 = int(num1)
-print(type(num1))
 
-num2 = 10
-num2 = float(num2)
-print(type(num2))
+def vasriables_tipo():
+    """
+    Muestra el tipo de datos que
+    contiene una variable
+    """
+    num1 = 7.5
+    num1 = int(num1)
+    print(type(num1))
 
-num1 = "7.5"
-num2 = "10"
-print(float(num1) + int(num2))
+    num2 = 10
+    num2 = float(num2)
+    print(type(num2))
+
+    num1 = "7.5"
+    num2 = "10"
+    print(float(num1) + int(num2))
